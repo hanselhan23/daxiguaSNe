3ef11bc1b2df47cea44caae695d476e3a9066a16
韩叙 <hanxu23@mails.tsinghua.edu.cn>
2025-11-22 12:56:16 +0800

    let adLink = false

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -35,7 +35,7 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = '';
+let adLink = false;
 
 // 修改网页标题：将 "合成超新星" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成超新星';
